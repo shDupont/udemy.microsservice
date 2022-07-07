9b1da191bb754d90a07eb83e4e8be7f15fa56235
Letícia Dupont <leticia.dupont20@gmail.com>
2022-07-07 17:48:01 -0300

    Initial commit

---
+++ src/test/java/com/dupont/microsservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dupont.microsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
