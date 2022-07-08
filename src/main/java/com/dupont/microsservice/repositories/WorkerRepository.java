1845bdb959925918add8f6785462b8622692ac9c
Letícia Dupont <leticia.dupont20@gmail.com>
2022-07-08 10:50:09 -0300

    fix

--- src/main/java/com/dupont/microsservice/repositories/WorkerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dupont.microsservice.repositories;
-
-import com.dupont.microsservice.entities.Worker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-
-}
